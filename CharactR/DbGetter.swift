923597f3859de00f6090ba118d8d4257b7821e91
MADELINE ALEXANDRE <madelina@mac02-m102.local>
2019-01-10 15:57:44 +0100

    creation of the back for a unique dictionary for each user in progress...

--- CharactR/DbGetter.swift
+++ CharactR/DbGetter.swift
@@ -17,6 +17,7 @@ class DbGetter{
     
     private let symbol_table = Table("symbol")
     private let symbol_id = Expression<Int>("id")
+    private let symbol_dictIdFK = Expression<Int>("dictId")
     private let symbol_image = Expression<String>("image")
     private let symbol_signification = Expression<String>("signification")
     private let symbol_commentary = Expression<String>("commentary")
@@ -27,6 +28,10 @@ class DbGetter{
     private let user_hash = Expression<String>("passwd_hash")
     private let user_email = Expression<String>("email")
     private var table_created:Bool = false
+    
+    private let dict_usr_table = Table("dict_usr_table")
+    private let dict_usr_dictId = Expression<Int>("dictId")
+    private let dict_usr_usrIdFK = Expression<Int>("userId")
     
     private init(){
         do {
@@ -54,8 +59,11 @@ class DbGetter{
             let createTableSymbol = self.symbol_table.create { table in
                 table.column(self.symbol_id, primaryKey: .autoincrement)
                 table.column(self.symbol_image)
+                table.column(self.symbol_dictIdFK)
                 table.column(self.symbol_signification)
                 table.column(self.symbol_commentary)
+                
+                table.foreignKey(self.symbol_dictIdFK, references: dict_usr_table, self.dict_usr_dictId, delete: .cascade)
             }
             let createTableUser = self.user_table.create{ table in
                 table.column(self.user_id, primaryKey: .autoincrement)
@@ -63,9 +71,16 @@ class DbGetter{
                 table.column(self.user_hash)
                 table.column(self.user_email, unique: true)
             }
+            let createDictUsrTable = self.dict_usr_table.create{ table in
+                table.column(self.dict_usr_dictId, primaryKey: true)
+                table.column(self.dict_usr_usrIdFK, primaryKey: .autoincrement)
+                
+                table.foreignKey(self.dict_usr_usrIdFK, references: user_table, self.user_id, delete:.cascade)
+            }
             do{
-                try database.run(createTableSymbol)
                 try database.run(createTableUser)
+                try database.run(createDictUsrTable)
+                try database.run(createTableSymbol)
                 self.insertFakeDatas()
             }catch{
                 print (error)
@@ -75,11 +90,12 @@ class DbGetter{
     }
     
     public func insertFakeDatas(){
+        self.createUserAccount(username: "Michel", password: "Ziboss", email: "Michelleking@gmail.com")
         self.insertSymbol(s: Symbol(symbol: "汉语", signification: "qzfqzfqf", commentary: ""))
         self.insertSymbol(s: Symbol(symbol: "Ye", signification: "qzfqzfqf", commentary: ""))
         self.insertSymbol(s: Symbol(symbol: "Ya", signification: "qzfqzfqf", commentary: ""))
         self.insertSymbol(s: Symbol(symbol: "Yi", signification: "qzfqzfqf", commentary: ""))
-        self.createUserAccount(username: "Michel", password: "Ziboss", email: "Michelleking@gmail.com")
+        
         print("fake data loaded")
     }
     
@@ -174,5 +190,22 @@ class DbGetter{
             print (error)
         }
         return false
+    }
+    
+    public func createDict(user: String) -> Int{
+        let selectUser = user_table.filter(user_name == user)
+        
+        do{
+            if try self.database.scalar(selectUser.count) == 1 {
+                for user in try database.prepare(selectUser){
+                    let id = user[user_id]
+                    let insert = self.dict_usr_table.insert(dict_usr_usrIdFK <- id);
+                    try self.database.run(insert)
+                }
+            }
+        }catch{
+            print(error)
+        }
+        return 0
     }
 }
